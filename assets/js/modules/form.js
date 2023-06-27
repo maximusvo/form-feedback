130cc63764e1ffa4c9197b43ef5d5547c2334517
maximusvo <maximilian-voigt@gmx.de>
2023-06-27 18:55:59 +0200

    fixed button actions

--- assets/js/modules/form.js
+++ assets/js/modules/form.js
@@ -1,15 +1,15 @@
-/*var status = 0;
+var status = 0;
 var button = document.getElementById("button");
 button.addEventListener("click", buttonAction);
 const position = localStorage.getItem("position");
 const grade = localStorage.getItem("grade");
-*/
+
 function feedbackForm() {
     const position = localStorage.getItem("position");
     const grade = localStorage.getItem("grade");
     var feedbackOne, feedbackTwo;
 
-    if (typeof position !== 'undefined' && position !== null){
+    //if (typeof position !== 'undefined' && position !== null){
         if (grade == "1") {
             feedbackOne = "Nur informieren? Nicht so geil!";
         }
@@ -24,14 +24,14 @@ function feedbackForm() {
         }
         document.getElementById("feedbackOne").innerHTML = feedbackOne;
         document.getElementById("feedbackTwo").innerHTML = feedbackTwo;
-        document.getElementById("button").setAttribute('onclick', clearForm());
-        document.getElementById("button").innerHTML = "Ergebnis löschen";
-        }
-    else {
+        //document.getElementById("button").setAttribute('onclick', clearForm());
+        //document.getElementById("button").innerHTML = "Ergebnis löschen";
+    //    }
+    /*else {
         document.getElementById("feedbackOne").innerHTML = "Es liegen keine Ergebnisse vor.";
         document.getElementById("button").innerHTML = "Zurück zum Tool";
         document.getElementById("button").setAttribute('onclick', formRestart());
-    }
+    }*/
 
     return;
 }
@@ -44,9 +44,11 @@ function saveForm() {
 }
 
 function clearForm() {
-
+    document.getElementById("feedbackOne").innerHTML = "";
+    document.getElementById("feedbackTwo").innerHTML = "";
     localStorage.clear();
-    //button.addEventListener("click", buttonAction);
+    document.getElementById("button").innerHTML = "Zurück zum Tool";
+    button.addEventListener("click", formRestart);
     return;
 }
 
@@ -54,12 +56,12 @@ function formRestart() {
     document.getElementById("button").setAttribute('href', "/form.html");
     return;
 }
-/*
+
 function buttonAction (){
     if (typeof position !== 'undefined' && position !== null){
         feedbackForm();
-        //button.addEventListener("click", clearForm);
-        //document.getElementById("button").innerHTML = "Ergebnis löschen";
+        button.addEventListener("click", clearForm);
+        document.getElementById("button").innerHTML = "Ergebnis löschen";
     }
     else{
         document.getElementById("feedbackOne").innerHTML = "Es liegen keine Ergebnisse vor.";
@@ -70,4 +72,3 @@ function buttonAction (){
 
 }
 
-*/
