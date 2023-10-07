54d18f32def28f1e6caaa43bae48c304e6550cb0
maximusvo <maximilian-voigt@gmx.de>
2023-10-07 17:40:26 +0200

    added sources to printer

--- assets/js/modules/form_scripts.js
+++ assets/js/modules/form_scripts.js
@@ -643,18 +643,3 @@ if(document.getElementsByClassName("collapsible") != null){
       });
     }
 }
-
-function printFile () {
-
-		var pdf = new jsPDF('p', 'pt', 'letter');
-		pdf.html(document.getElementById('html'), {
-			callback: function (pdf) {
-				var iframe = document.createElement('iframe');
-				iframe.setAttribute('style', 'position:absolute;right:0; top:0; bottom:0; height:100%; width:500px');
-				document.body.appendChild(iframe);
-				iframe.src = pdf.output('datauristring');
-			}
-		}
-		);
-
-}
